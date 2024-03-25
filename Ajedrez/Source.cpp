c7a14f74546ba6f1c1d9c710872772c9c588df85
Eduard Sagnier <eduard.sagnier@enti.cat>
2024-03-25 22:08:08 +0100

    Piezas se ponen en el  mapa.

--- Ajedrez/Source.cpp
+++ Ajedrez/Source.cpp
@@ -20,34 +20,57 @@ void meter_piezas() {
 
 }
 
-char crear_tabla() {
+void crear_tabla(char mapa[NUM_FILAS][NUM_COLUMNAS]) {
 
-	char mapa[NUM_FILAS][NUM_COLUMNAS];
-	for (int i = 0; i < NUM_FILAS; i++)
-	{
+
+	for (int i = 0; i < NUM_FILAS; i++) {
 		for (int j = 0; j < NUM_COLUMNAS; j++) {
 
-			if (i == 0 && j != 0)
-			{
-				mapa[i][j] = '0' + j; // De esta manera me permite convertir el int en char.
+			if (i == 0 && j != 0) {
+			// De esta manera me permite convertir el int en char.
+				mapa[i][j] = '0' + j;
 			}
-			else if (j == 0 && i != 0)
-			{
+			else if (j == 0 && i != 0) {
 				mapa[i][j] = '0' + i;
 			}
-			else
-			{
+			else {
 				mapa[i][j] = SIN_NADA;
 			}
 
 		}
 	}
 	mapa[0][0] = ESPACIO;
+
+}
+void poner_piezas(char mapa[NUM_FILAS][NUM_COLUMNAS]) {
+
+	char piezas_mayus[] = { 'T', 'C', 'A', 'D', 'R', 'A', 'C', 'T' };
+	char peones_mayus[] = { 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' };
+	char piezas_minus[] = { 't', 'c', 'a', 'd', 'r', 'a', 'c', 't' };
+	char peones_minus[] = { 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' };
+
+	for (int i = 0; i < NUM_FILAS; i++) {
+		for (int j = 0; j < NUM_COLUMNAS; j++) {
+
+			if (i == 7 && j != 0){
+				mapa[i][j] = peones_mayus[j - 1];
+			}
+			if (i == 8 && j != 0){
+				mapa[i][j] = piezas_mayus[j - 1];
+			}
+			if (i == 1 && j != 0){
+				mapa[i][j] = piezas_minus[j - 1];
+			}
+			if (i == 2 && j != 0) {
+				mapa[i][j] = peones_minus[j - 1];
+			}
+		}
+	}
+
+
 }
 
-void sacar_mapa() {
-
-
+void sacar_mapa(char mapa[NUM_FILAS][NUM_COLUMNAS]) {
 
 
 
@@ -61,5 +84,15 @@ void sacar_mapa() {
 		}
 		cout << endl;
 	}
+}
+
+void main() {
+
+	char mapa[NUM_FILAS][NUM_COLUMNAS];
+	crear_tabla(mapa);
+	poner_piezas(mapa);
+	sacar_mapa(mapa);
+
+
 }
 
