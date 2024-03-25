f2180a04ae7397bf944f318da1955b6174752813
Eduard Sagnier <eduard.sagnier@enti.cat>
2024-03-25 22:28:44 +0100

    He creado el cpp para el codigo del mapa / tablero y he creado el header para las funciones del mapa. He añadido para crear la logica del juego.

---
+++ Ajedrez/mapa.cpp
@@ -0,0 +1,74 @@
+#define NUM_FILAS 9
+#define NUM_COLUMNAS 9
+
+#define SIN_NADA '*'
+#define ESPACIO ' '
+#include <iostream>
+
+using namespace std;
+
+void crear_tabla(char mapa[NUM_FILAS][NUM_COLUMNAS]) {
+
+
+	for (int i = 0; i < NUM_FILAS; i++) {
+		for (int j = 0; j < NUM_COLUMNAS; j++) {
+
+			if (i == 0 && j != 0) {
+				// De esta manera me permite convertir el int en char.
+				mapa[i][j] = '0' + j;
+			}
+			else if (j == 0 && i != 0) {
+				mapa[i][j] = '0' + i;
+			}
+			else {
+				mapa[i][j] = SIN_NADA;
+			}
+
+		}
+	}
+	mapa[0][0] = ESPACIO;
+
+}
+void poner_piezas(char mapa[NUM_FILAS][NUM_COLUMNAS]) {
+
+	char piezas_minus[] = { 't', 'h', 'b', 'q', 'k', 'b', 'h', 't' };
+	char peones_minus[] = { 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' };
+	char piezas_mayus[] = { 'T', 'H', 'B', 'Q', 'K', 'B', 'H', 'T' };
+	char peones_mayus[] = { 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' };
+
+
+	for (int i = 0; i < NUM_FILAS; i++) {
+		for (int j = 0; j < NUM_COLUMNAS; j++) {
+
+			if (i == 7 && j != 0) {
+				mapa[i][j] = peones_mayus[j - 1];
+			}
+			if (i == 8 && j != 0) {
+				mapa[i][j] = piezas_mayus[j - 1];
+			}
+			if (i == 1 && j != 0) {
+				mapa[i][j] = piezas_minus[j - 1];
+			}
+			if (i == 2 && j != 0) {
+				mapa[i][j] = peones_minus[j - 1];
+			}
+		}
+	}
+
+
+}
+
+void sacar_mapa(char mapa[NUM_FILAS][NUM_COLUMNAS]) {
+
+
+
+	for (int i = 0; i < NUM_FILAS; i++) {
+		for (int j = 0; j < NUM_COLUMNAS; j++) {
+
+			cout << mapa[i][j] << ESPACIO;
+
+
+		}
+		cout <<endl;
+	}
+}
---
+++ Ajedrez/mapa.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define NUM_FILAS 9
+#define NUM_COLUMNAS 9
+
+
+void crear_tabla(char mapa[NUM_FILAS][NUM_COLUMNAS]);
+void poner_piezas(char mapa[NUM_FILAS][NUM_COLUMNAS]);
+void sacar_mapa(char mapa[NUM_FILAS][NUM_COLUMNAS]);
